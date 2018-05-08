e352b8c5938856b5041a02009d18d08e1c8f69d7
MrCheka <stasbajko@gmail.com>
2018-05-08 22:57:34 +0300

    service added

---
+++ Deanery/Classes/Service.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace Deanery.Classes
+{
+    public static class Service
+    {
+        public static User CurrentUser { get; set; }
+
+        public static string GetConnectionString()
+        {
+            string connectionString = string.Empty;
+
+            ConnectionStringSettings settings =
+                ConfigurationManager.ConnectionStrings["DeaneryDbConnectionString"];
+
+            if (settings != null)
+                connectionString = settings.ConnectionString;
+
+            return connectionString;
+        }
+
+        public static SqlConnection OpenConnection()
+        {
+            string connectionString = GetConnectionString();
+            var connection = new SqlConnection(connectionString);
+            try
+            {
+                connection.Open();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка!");
+            }
+            return connection;
+        }
+
+        public static void CloseConnection(SqlConnection connection)
+        {
+            try
+            {
+                connection.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка!");
+            }
+        }
+    }
+}
